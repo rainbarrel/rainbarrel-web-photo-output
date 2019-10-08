b4cf12ebc21ccc08696e27600d42cb0baffea928
rainbarrel <rainbarrel57@gmail.com>
2019-10-08 00:36:08 -0700

    listen for bookend start & stop commands successfully (output-side)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import Raindrop from './components/Raindrop';
+import Default from './components/Default';
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import { Spin } from 'antd';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="app">
-      <Raindrop />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { beginShowingPhotos: null };
+  }
+
+  componentDidMount() {
+    const db = Firebase.firestore();
+    const currentBookendRef = db.collection('bookends').doc('current');
+
+    currentBookendRef.onSnapshot((doc) => {
+      const bookend = doc.data().bookend;
+      const beginShowingPhotos = (bookend === 'start');
+      this.setState({ beginShowingPhotos: beginShowingPhotos });
+    });
+  }
+
+  render() {
+    const { beginShowingPhotos } = this.state;
+
+    let view;
+    if (beginShowingPhotos === null) {
+      view = <Spin size='large' />;
+    } else {
+      view = beginShowingPhotos ? <Raindrop /> : <Default />;
+    }
+
+    return (
+      <div className="app">
+        {view}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Default.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Default = () => {
+  const message = "Welcome to Rainbarrel, where we will use the power of your voice to view your beautiful photos sent from all around the world to you."
+
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Default;
